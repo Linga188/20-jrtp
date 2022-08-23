6e606251daf7a9e3f5cb47a3158191a675027801
Your Name <you@example.com>
2022-08-24 04:15:10 +0530

    first commit

---
+++ src/main/java/com/ashok/security/SecurityService.java
@@ -0,0 +1,40 @@
+package com.ashok.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class SecurityService {
+
+	public static String encodePassword(String txt) {
+		Encoder encoder = Base64.getEncoder();
+		return encoder.encodeToString(txt.getBytes());
+		
+		
+	}
+	public static String decode(String txt) {
+		Decoder decoder = Base64.getDecoder();
+		byte[] decode = decoder.decode(txt);
+		return new String(decode);
+	}
+	
+	public static void main(String[] args) {
+		SecurityService securityService=new SecurityService();
+		String encodePassword = SecurityService.encodePassword("Gadilingappa");
+		System.out.println(encodePassword);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
