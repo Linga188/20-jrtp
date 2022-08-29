b0f7a47701fff6be8fea896bbad3655614b166e9
Your Name <you@example.com>
2022-08-29 13:14:25 +0530

    remove unwanted

--- src/main/java/com/ashok/security/SecurityService.java
+++ src/main/java/com/ashok/security/SecurityService.java
@@ -5,7 +5,7 @@ import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
 
 public class SecurityService {
-
+	
 	public static String encodePassword(String txt) {
 		Encoder encoder = Base64.getEncoder();
 		return encoder.encodeToString(txt.getBytes());
